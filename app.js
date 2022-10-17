3ef2987262ca5ba3fa7a7fc1f114300d3ed4a45a
Edward Espinoza <edwesti2710@gmail.com>
2022-10-16 22:18:24 -0500

    add elements

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors');
 require('dotenv').config()
 const app = express()
 const swaggerUi = require('swagger-ui-express')
@@ -7,6 +8,25 @@ const swaggerFile = require('./swagger-output.json')
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', require('./src/routes'))
+
+var allowlist = ['http://example1.com', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.get('/products/:id', cors(corsOptionsDelegate), function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for an allowed domain.' })
+})
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+})
 
 const PORT = process.env.PORT || 3000
 
