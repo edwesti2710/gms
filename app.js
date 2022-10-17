029531396d60a461111dd65da32a1732b66dfc54
Edward Espinoza <edwesti2710@gmail.com>
2022-10-16 22:53:33 -0500

    add elements

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express()
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/', require('./src/routes'))
